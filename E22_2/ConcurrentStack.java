0096f88e936d21577a1ba2125f7ec31ebab4b2c8
Masud_Rana <masudranaorg71@gmail.com>
2023-10-02 11:53:09 +0600

    Second Problem

---
+++ E22_2/ConcurrentStack.java
@@ -0,0 +1,19 @@
+package E22_2;
+
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+public class ConcurrentStack<T> {
+    private ConcurrentLinkedDeque<T> stack = new ConcurrentLinkedDeque<>();
+
+    public void push(T item) {
+        stack.push(item);
+    }
+
+    public T pop() {
+        return stack.poll();
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
